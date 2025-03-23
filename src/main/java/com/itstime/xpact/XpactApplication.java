e146d32f2476afa90dbdec810e1db6ad5058ec4a
Wonjae Lim <yyytir777@gmail.com>
2025-03-23 14:06:05 +0900

    chore : Init project

---
+++ src/main/java/com/itstime/xpact/XpactApplication.java
@@ -0,0 +1,13 @@
+package com.itstime.xpact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpactApplication.class, args);
+    }
+
+}
