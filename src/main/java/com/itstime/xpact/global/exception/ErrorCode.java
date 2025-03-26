2208e27e3720cc0e16f50472f11d0c2b5e8065a8
Wonjae Lim <yyytir777@gmail.com>
2025-03-27 00:31:20 +0900

    feature/#6 : Exception클래스 추가 및 ErrorCode 추가 생성

--- src/main/java/com/itstime/xpact/global/exception/ErrorCode.java
+++ src/main/java/com/itstime/xpact/global/exception/ErrorCode.java
@@ -8,8 +8,14 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    TEST(HttpStatus.INTERNAL_SERVER_ERROR, "500 에러", "Test Error"),;
+    TEST(HttpStatus.INTERNAL_SERVER_ERROR, "500 에러", "Test Error"),
 
+    EXPERIENCE_NOT_EXISTS(HttpStatus.NO_CONTENT, "EXP001", "Experience Not Exists"),
+    INVALID_FORMTYPE(HttpStatus.BAD_REQUEST, "EXP002", "Invalid FormType"),
+    INVALID_STATUS(HttpStatus.BAD_REQUEST, "EXP003", "Invalid Status"),
+
+
+    ;
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
