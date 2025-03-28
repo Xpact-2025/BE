0f781ae68680e1fdac34aaf0b19c0e7ae817be17
kite_U <yurry45@naver.com>
2025-03-28 13:15:04 +0900

    feature/#3 : Member ID를 기반으로 Authentication 등록 기능 구현

--- src/main/java/com/itstime/xpact/global/auth/MemberAuthentication.java
+++ src/main/java/com/itstime/xpact/global/auth/MemberAuthentication.java
@@ -1,17 +1,25 @@
 package com.itstime.xpact.global.auth;
 
+import com.itstime.xpact.domain.member.entity.Member;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Collection;
+import java.util.Collections;
 
 public class MemberAuthentication extends UsernamePasswordAuthenticationToken {
-    public MemberAuthentication(Object principal, Object credentials) {
-        super(principal, credentials);
+
+    private final Member member;
+
+    public MemberAuthentication(Member member, Object credentials) {
+        super(member.getId(), credentials); // principal을 memberId로 설정
+        this.member = member;
     }
 
-    public MemberAuthentication(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
-        super(principal, credentials, authorities);
+    public MemberAuthentication(Member member, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+        super(member.getId(), credentials, authorities);
+        this.member = member;
     }
 
     @Override
@@ -21,6 +29,27 @@ public class MemberAuthentication extends UsernamePasswordAuthenticationToken {
 
     @Override
     public Object getPrincipal() {
-        return super.getPrincipal();
+        return member.getId();
+    }
+
+    public Member getMember() {
+        return this.member;
+    }
+
+    public static MemberAuthentication createMemberAuthentication(Member member) {
+
+        Collection<? extends GrantedAuthority> authorities =
+                Collections.singletonList(new SimpleGrantedAuthority(member.getRole().name()));
+
+        return new MemberAuthentication(
+                member,
+                null,
+                authorities
+        );
+    }
+
+    @Override
+    public Collection<GrantedAuthority> getAuthorities() {
+        return super.getAuthorities();
     }
 }
