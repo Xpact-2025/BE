6d98660757c974e167c5142ff2ec9f00c5cc4afb
kite_U <yurry45@naver.com>
2025-03-28 16:51:40 +0900

    feature/#3 : 쿠키에 저장된 refresh token 유효성 검증 후 memberID 반환 로직 구현

--- src/main/java/com/itstime/xpact/global/security/service/RefreshTokenService.java
+++ src/main/java/com/itstime/xpact/global/security/service/RefreshTokenService.java
@@ -1,8 +1,12 @@
 package com.itstime.xpact.global.security.service;
 
+import com.itstime.xpact.domain.member.repository.MemberRepository;
+import com.itstime.xpact.global.auth.TokenProvider;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -10,10 +14,13 @@ import org.springframework.stereotype.Service;
 import java.util.concurrent.TimeUnit;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class RefreshTokenService {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private final TokenProvider tokenProvider;
+    private final MemberRepository memberRepository;
 
     @Value("${spring.jwt.refresh.expiration}")
     private long refreshTokenExpiration;
@@ -56,5 +63,38 @@ public class RefreshTokenService {
         refreshTokenCookie.setPath("/");
         refreshTokenCookie.setMaxAge(0); // 즉시 만료
         response.addCookie(refreshTokenCookie);
+    }
+
+    // 기록된 쿠키 조회 후 검증
+    public Long getMemberIdFromCookie(HttpServletRequest request) {
+
+        log.info("쿠키로부터 Refresh Token 찾기 시작...");
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies == null || cookies.length == 0) {
+            log.warn("쿠키가 비어 있습니다.");
+            // TODO : CustomException으로 변경하기
+            throw new RuntimeException("쿠키가 존재하지 않습니다.");
+        }
+
+        for (Cookie cookie : cookies) {
+            if ("refreshToken".equals(cookie.getName())) {
+                String refreshToken = cookie.getValue();
+                log.info("Refresh Token 발견. 유효성 검사 시작...");
+
+                if (tokenProvider.validationToken(refreshToken).getSuccess()) {
+                    Long memberId = tokenProvider.getMemberIdFromToken(refreshToken);
+                    log.info("유효한 Refresh token입니다. Member Id: {}", memberId);
+                    return memberId;
+                } else {
+                    log.warn("유효하지 않은 Refresh Token입니다.");
+                    // TODO : CustomException으로 변경
+                    throw new RuntimeException("유효하지 않은 Refresh Token입니다.");
+                }
+            }
+        }
+        log.warn("Refresh token 쿠키를 찾을 수 없습니다.");
+        // TODO : CustomException으로 변경
+        throw new RuntimeException("refreshToken 쿠키가 없습니다.");
     }
 }
