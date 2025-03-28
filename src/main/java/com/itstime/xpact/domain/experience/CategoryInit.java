5669717464aaf834d7e19cfe4e952295d49ca596
Wonjae Lim <yyytir777@gmail.com>
2025-03-28 10:11:42 +0900

    feature/#6 : Category 데이터 추가 (임시 Member 추가)

--- src/main/java/com/itstime/xpact/domain/experience/CategoryInit.java
+++ src/main/java/com/itstime/xpact/domain/experience/CategoryInit.java
@@ -3,12 +3,17 @@ package com.itstime.xpact.domain.experience;
 import com.itstime.xpact.domain.experience.common.ExperienceType;
 import com.itstime.xpact.domain.experience.entity.Category;
 import com.itstime.xpact.domain.experience.repository.CategoryRepository;
+import com.itstime.xpact.domain.member.common.Role;
+import com.itstime.xpact.domain.member.common.Type;
+import com.itstime.xpact.domain.member.entity.Member;
+import com.itstime.xpact.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,11 +23,14 @@ import java.util.List;
 public class CategoryInit {
 
     private final CategoryRepository categoryRepository;
+    private final MemberRepository memberRepository;
 
     @Bean
     public CommandLineRunner init() {
         return args -> {
-            if(categoryRepository.count() == 0) {
+
+            long categoryCount = categoryRepository.count();
+            if(categoryCount == 0) {
                 List<Category> categoryList = Arrays.stream(ExperienceType.values())
                         .map(name -> Category.builder().name(name.toString()).build())
                         .toList();
@@ -31,6 +39,24 @@ public class CategoryInit {
                 log.info("Saved {} category", categoryList.size());
             } else {
                 log.info("Found {} category", categoryRepository.count());
+            }
+
+            long memberCount = memberRepository.count();
+            if(memberCount == 0) {
+                Member memeber = Member.builder()
+                        .email("demo")
+                        .name("demo")
+                        .role(Role.ROLE_USER)
+                        .type(Type.FORM)
+                        .imgurl("demo")
+                        .inactiveDate(LocalDate.now())
+                        .birthDate(LocalDate.now())
+                        .build();
+
+                memberRepository.save(memeber);
+                log.info("Saved {} member", memberRepository.count());
+            } else {
+                log.info("Found {} member", memberRepository.count());
             }
         };
     }
