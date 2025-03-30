1fbf2952d7a1c3190e0f3649d967076986abe972
kite_U <yurry45@naver.com>
2025-03-30 20:36:47 +0900

    fix/#20 : CustomException 클래스 추가로 예외 처리 통일

---
+++ src/main/java/com/itstime/xpact/global/exception/CustomException.java
@@ -0,0 +1,19 @@
+package com.itstime.xpact.global.exception;
+
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public static CustomException of(ErrorCode errorCode) {
+        return new CustomException(errorCode);
+    }
+}
