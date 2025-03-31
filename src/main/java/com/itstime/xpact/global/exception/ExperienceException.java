8c35f27d310ebdcb0b135cac6bab467848cb2f90
kite_U <yurry45@naver.com>
2025-03-31 23:34:07 +0900

    fix/#20 : 서비스 로직 별 생성한 Exception 클래스 제거 ( CustomException으로 통일 )

--- src/main/java/com/itstime/xpact/global/exception/ExperienceException.java
+++
@@ -1,8 +0,0 @@
-package com.itstime.xpact.global.exception;
-
-public class ExperienceException extends BusinessException {
-
-    public ExperienceException(ErrorCode errorCode) {
-        super(errorCode);
-    }
-}
