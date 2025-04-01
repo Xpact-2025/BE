f5cbfc436bf450fadd13211a9cf2172e1015c982
kite_U <yurry45@naver.com>
2025-04-01 14:17:22 +0900

    feature/#20 : Authentication이 비어있을 때와 Member가 존재하지 않을 때의 경우 분리하여 예외처리

--- src/main/java/com/itstime/xpact/global/auth/SecurityProvider.java
+++ src/main/java/com/itstime/xpact/global/auth/SecurityProvider.java
@@ -1,7 +1,9 @@
 package com.itstime.xpact.global.auth;
 
+import com.itstime.xpact.domain.member.entity.Member;
 import com.itstime.xpact.global.exception.CustomException;
 import com.itstime.xpact.global.exception.ErrorCode;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +11,18 @@ import org.springframework.stereotype.Component;
 public class SecurityProvider {
 
     public Long getCurrentMemberId() {
-        MemberAuthentication authentication = (MemberAuthentication) SecurityContextHolder.getContext().getAuthentication();
+        Authentication authentication;
+        authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        try {
-            return authentication.getMember().getId();
-        } catch (Exception e) {
+        if (!(authentication instanceof MemberAuthentication memberAuth)) {
+            throw CustomException.of(ErrorCode.UNAUTHORIZED_ACCESS);
+        }
+
+        Member member = memberAuth.getMember();
+
+        if (member == null) {
             throw CustomException.of(ErrorCode.MEMBER_NOT_EXISTS);
         }
+        return member.getId();
     }
 }
