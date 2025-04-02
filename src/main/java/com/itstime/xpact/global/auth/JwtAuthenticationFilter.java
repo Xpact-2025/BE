a713588be726e4cbb0b26416051c73a5c2d5794d
kite_U <yurry45@naver.com>
2025-04-02 23:30:58 +0900

    feature/#15 : SecurityFilter path 수정

--- src/main/java/com/itstime/xpact/global/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/itstime/xpact/global/auth/JwtAuthenticationFilter.java
@@ -40,7 +40,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String path = request.getRequestURI();
         System.out.println("Request path: " + path);
         boolean shouldNotFilter = path.startsWith("/auth/signup") ||
-                path.startsWith("/auth/login") ||
+                path.startsWith("/auth/login/") ||
                 path.startsWith("/v3/") ||
                 path.startsWith("/swagger-ui/");
         System.out.println("Should not filter: " + shouldNotFilter);
