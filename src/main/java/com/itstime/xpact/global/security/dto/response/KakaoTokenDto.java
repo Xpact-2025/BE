5925f3681a500c233784cb71bf3614b3fa5f862d
kite_U <yurry45@naver.com>
2025-04-02 23:30:02 +0900

    feature/#15 : KakaoTokenDto 클래스 생성

---
+++ src/main/java/com/itstime/xpact/global/security/dto/response/KakaoTokenDto.java
@@ -0,0 +1,21 @@
+package com.itstime.xpact.global.security.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class KakaoTokenDto {
+
+    @JsonProperty("token_type")
+    public String tokenType;
+    @JsonProperty("access_token")
+    public String accessToken;
+    @JsonProperty("expires_in")
+    public int expiresIn;
+    @JsonProperty("refresh_token")
+    public String refreshToken;
+    @JsonProperty("refresh_token_expires_in")
+    public int refreshTokenExpiresIn;
+    @JsonProperty("scope")
+    public String scope;
+}
