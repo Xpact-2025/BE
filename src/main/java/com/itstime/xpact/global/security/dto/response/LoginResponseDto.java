2bc020b2c878cbc2c9fa2e17c5ad07548b6d2bd2
kite_U <yurry45@naver.com>
2025-04-02 23:32:44 +0900

    chore/#15 : LoginResponseDto Builder 어노테이션 추가

--- src/main/java/com/itstime/xpact/global/security/dto/response/LoginResponseDto.java
+++ src/main/java/com/itstime/xpact/global/security/dto/response/LoginResponseDto.java
@@ -1,5 +1,9 @@
 package com.itstime.xpact.global.security.dto.response;
 
+import com.itstime.xpact.domain.member.entity.Member;
+import lombok.Builder;
+
+@Builder
 public record LoginResponseDto (
         String accessToken
 ){
