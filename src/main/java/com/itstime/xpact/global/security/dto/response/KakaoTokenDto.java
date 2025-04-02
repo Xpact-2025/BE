0fc683a0a7ec277e6d457adaa2a217f2fd60c07b
kite_U <yurry45@naver.com>
2025-04-03 03:55:27 +0900

    feature/#15 : KakaoTokenDto Getter 추가

--- src/main/java/com/itstime/xpact/global/security/dto/response/KakaoTokenDto.java
+++ src/main/java/com/itstime/xpact/global/security/dto/response/KakaoTokenDto.java
@@ -2,7 +2,9 @@ package com.itstime.xpact.global.security.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoTokenDto {
 
