fea64202f0a068117fa008df6a6c35b6f6723f9b
kite_U <yurry45@naver.com>
2025-04-03 03:56:43 +0900

    feature/#15 : RestTemplate -> WebClient 형식으로 변경

--- src/main/java/com/itstime/xpact/global/security/util/KakaoUtil.java
+++ src/main/java/com/itstime/xpact/global/security/util/KakaoUtil.java
@@ -10,11 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
-import java.util.Arrays;
 
 @Component
 @Slf4j
@@ -27,82 +25,68 @@ public class KakaoUtil {
     @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
     private static String redirectUri;
 
-    @Value("${spring.security.oauth2.client.provider.kakao.authorization-uri}")
-    private static String authorizationUri;
+    private static final String KAUTH_TOKEN_URL_HOST = "https://kauth.kakao.com"; // 카카오 인증 URI
+    private static final String KAUTH_USER_URL_HOST = "https://kapi.kakao.com"; // 카카오 사용자 정보 요청 URI
 
-    @Value("${spring.security.oauth2.client.provider.kakao.user-info-uri}")
-    private String userInfoUri;
-
-    @Value("${spring.security.oauth2.client.provider.kakao.token-uri}")
-    private String tokenUri;
+    // 인증코드 URL
+    public String buildLoginUrl() {
+        return String.format(
+                "%s/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code&prompt=login",
+                KAUTH_TOKEN_URL_HOST, clientId, redirectUri
+        );
+    }
 
     // 카카오로부터 AccessToken 받아오기
-    public String reqeustToken(String authorizationCode) {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new org.springframework.http.HttpHeaders();
-
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type", "authorization_code");
-        params.add("client_id", clientId);
-        params.add("redirect_uri", redirectUri);
-        params.add("code", authorizationCode);
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
-
-        ResponseEntity<KakaoTokenDto> response =
-                restTemplate.exchange(
-                        tokenUri,
-                        HttpMethod.POST,
-                        request,
-                        KakaoTokenDto.class
-                );
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        KakaoTokenDto kakaoToken = null;
+    public KakaoTokenDto requestAccessToken(String authorizationCode) {
 
         try {
-            kakaoToken = objectMapper.readValue(
-                    response.getBody().accessToken, KakaoTokenDto.class
-            );
-        } catch (JsonProcessingException e) {
-            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
+            return createWebClient(KAUTH_TOKEN_URL_HOST)
+                    .post()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/oauth/token")
+                            .queryParam("grant_type", "authorization_code")
+                            .queryParam("client_id", clientId)
+                            .queryParam("redirect_uri", redirectUri)
+                            .queryParam("code", authorizationCode)
+                            .build())
+                    .retrieve()
+                    .bodyToMono(KakaoTokenDto.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            log.error("카카오 서버로 AccessToken 요청 중 오류 : {} ", e.getResponseBodyAsString());
+            throw CustomException.of(ErrorCode.ACCESS_TOKEN_REQUEST_FAILED);
         }
-        return kakaoToken.accessToken;
     }
 
     // 카카오로부터 Profile 얻어오기
-    public KakaoInfoResponseDto.KakaoAccount requestProfile(String token) throws CustomException {
-
-        RestTemplate restTemplate2 = new RestTemplate();
-        HttpHeaders headers2 = new HttpHeaders();
-
-        headers2.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
-        headers2.add("Authorization", "Bearer " + token);
-
-        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers2);
-
-        ResponseEntity<String> response2 = restTemplate2.exchange(
-                userInfoUri,
-                HttpMethod.GET,
-                kakaoProfileRequest,
-                String.class
-        );
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        KakaoInfoResponseDto.KakaoAccount profile = null;
-
+    public KakaoInfoResponseDto requestProfile(String token) {
         try {
-            profile = objectMapper.readValue(
-                    response2.getBody(), KakaoInfoResponseDto.KakaoAccount.class);
-        } catch (JsonProcessingException e) {
-            log.info(Arrays.toString(e.getStackTrace()));
-            throw CustomException.of(ErrorCode.INTERNAL_SERVER_ERROR);
+            return createWebClient(KAUTH_TOKEN_URL_HOST)
+                    .get()
+                    .uri("/v2/user/me")
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                    .retrieve()
+                    .bodyToMono(KakaoInfoResponseDto.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            log.error("카카오로부터 사용자 Info 요청 중 오류 : {} ", e.getResponseBodyAsString());
+            throw CustomException.of(ErrorCode.ACCESS_TOKEN_REQUEST_FAILED);
         }
+    }
 
-        return profile;
+    // WebClient를 이용한 요청
+    private WebClient createWebClient(String baseUrl) {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                // KAKAO open API 필수 Header
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8")
+                .build();
+    }
+
+    // KAKAO로부터 얻어온 token의 정보 콘솔 로그 출력
+    public void logTokenInfo(KakaoTokenDto tokenDto) {
+        log.info("[Kakao Token] Access Token: {}", tokenDto.getAccessToken());
+        log.info("[Kakao Token] Refresh Token: {}", tokenDto.getRefreshToken());
+        log.info("[Kakao Token] Scope: {}", tokenDto.getScope());
     }
 }
