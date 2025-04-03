2548e46eaec981985bf5129531f5b46eccae91ba
kite_U <yurry45@naver.com>
2025-04-03 13:06:50 +0900

    feature/#15 : Swagger Annotation 추가

--- src/main/java/com/itstime/xpact/global/security/controller/KakaoLoginController.java
+++ src/main/java/com/itstime/xpact/global/security/controller/KakaoLoginController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,15 +20,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "카카오 로그인 API Controller", description = "카카오 로그인 API")
 public class KakaoLoginController {
 
     private final KakaoLoginService kakaoLoginService;
 
+    @Operation(summary = "카카오 인증코드 받기", description = "카카오 인증코드를 받을 수 있는 url을 반환합니다.")
     @GetMapping("/url/kakao")
     public ResponseEntity<String> get() {
         return ResponseEntity.ok(kakaoLoginService.getLoginUrl());
     }
 
+
+    @Operation(summary = "카카오 로그인하기", description = "카카오 로그인 기능의 API 입니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "401", description = "카카오 서버로의 요청 실패", content = @Content(schema = @Schema(implementation = ErrorResponse.class),
                     examples = @ExampleObject(value = """
@@ -54,7 +59,6 @@ public class KakaoLoginController {
                             }
                             """)))
     })
-    @Operation(summary = "카카오 로그인하기", description = "카카오 로그인 기능의 API 입니다.")
     @PostMapping("/login/kakao")
     public ResponseEntity<RestResponse<?>> callback(
             @Parameter(description = "카카오로부터 얻은 인증 코드")
