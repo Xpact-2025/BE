740680f8a7b35a55ac095e4b9b2c4e6e619fac4e
kite_U <yurry45@naver.com>
2025-04-03 10:24:54 +0900

    fix/#15 : 중복 ErrorCode 제거

--- src/main/java/com/itstime/xpact/global/exception/ErrorCode.java
+++ src/main/java/com/itstime/xpact/global/exception/ErrorCode.java
@@ -23,7 +23,6 @@ public enum ErrorCode {
     INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "TE003", "유효하지 않은 토큰입니다."),
     EXPIRED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "TE004", "토큰의 기한이 만료되었습니다."),
     UNSUPPORTED_JWT_TOKEN(HttpStatus.UNAUTHORIZED, "TE005", "지원하지 않는 토큰 형식입니다."),
-    EMPTY_JWT(HttpStatus.BAD_REQUEST, "TE006", "토큰이 존재하지 않습니다."),
 
     // login
     UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "권한이 존재하지 않습니다"),
