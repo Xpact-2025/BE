09f356d720c2364a1ea0dc2b66168dc8973ecdc1
Wonjae Lim <yyytir777@gmail.com>
2025-04-05 00:22:32 +0900

    feature/#33 : ExperienceRepository 메서드 작성

---
+++ src/main/java/com/itstime/xpact/domain/experience/repository/ExperienceCustomRepositoryImpl.java
@@ -0,0 +1,43 @@
+package com.itstime.xpact.domain.experience.repository;
+
+import com.itstime.xpact.domain.experience.common.ExperienceType;
+import com.itstime.xpact.domain.experience.entity.Experience;
+import com.itstime.xpact.domain.experience.entity.QExperience;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ExperienceCustomRepositoryImpl implements ExperienceCustomRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public List<Experience> findAllByMemberIdAndType(Long memberId, String order, List<ExperienceType> types) {
+        QExperience experience = QExperience.experience;
+
+        OrderSpecifier<?> orderSpecifier = null;
+        if(order.equals("LATEST")) {
+            orderSpecifier = new OrderSpecifier<>(Order.DESC, experience.modifiedTime);
+        } else if(order.equals("OLDEST")) {
+            orderSpecifier = new OrderSpecifier<>(Order.ASC, experience.modifiedTime);
+        }
+
+        BooleanBuilder builder = new BooleanBuilder();
+        for (ExperienceType type : types) {
+            builder.or(experience.experienceType.eq(type));
+        }
+
+        return queryFactory.selectFrom(experience)
+                .where(experience.member.id.eq(memberId))
+                .where(builder)
+                .orderBy(orderSpecifier)
+                .fetch();
+    }
+
+}
