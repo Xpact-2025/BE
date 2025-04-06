eb90dd12f305079f19a29d9da09a11612863bf53
Wonjae Lim <yyytir777@gmail.com>
2025-04-07 00:58:55 +0900

    feature/#36 : enum클래스에 해당 타입인지 검증하는 validator생성

--- src/main/java/com/itstime/xpact/domain/experience/common/FormType.java
+++ src/main/java/com/itstime/xpact/domain/experience/common/FormType.java
@@ -1,5 +1,16 @@
 package com.itstime.xpact.domain.experience.common;
 
+import com.itstime.xpact.global.exception.CustomException;
+import com.itstime.xpact.global.exception.ErrorCode;
+
 public enum FormType {
-    STAR_FORM, SIMPLE_FORM
+    STAR_FORM, SIMPLE_FORM;
+
+    public static void validateFormType(String formType) {
+        try {
+            FormType.valueOf(formType);
+        } catch (Exception e) {
+            throw CustomException.of(ErrorCode.INVALID_FORMTYPE);
+        }
+    }
 }
