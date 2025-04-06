89a1745a752781579be586b3b996c2b6811d0c4b
Wonjae Lim <yyytir777@gmail.com>
2025-04-07 01:05:19 +0900

    fix/#36 : ExperienceUpdateRequestDto변경으로 인한 Experience 메서드 변경

--- src/main/java/com/itstime/xpact/domain/experience/entity/Experience.java
+++ src/main/java/com/itstime/xpact/domain/experience/entity/Experience.java
@@ -132,14 +132,14 @@ public class Experience extends BaseEntity {
     }
 
     public void updateToSimpleForm(ExperienceUpdateRequestDto updateRequestDto) {
-        this.status = updateRequestDto.getStatus();
-        this.formType = updateRequestDto.getFormType();
+        this.status = Status.valueOf(updateRequestDto.getStatus());
+        this.formType = FormType.valueOf(updateRequestDto.getFormType());
         this.title = updateRequestDto.getTitle();
         this.isEnded = updateRequestDto.getEndDate().isBefore(LocalDate.now());
         this.startDate = updateRequestDto.getStartDate();
         this.endDate = updateRequestDto.getEndDate();
         this.keyword = updateRequestDto.getKeyword();
-        this.experienceType = updateRequestDto.getExperienceType();
+        this.experienceType = ExperienceType.valueOf(updateRequestDto.getExperienceType());
         this.situation = null;
         this.task = null;
         this.action = null;
@@ -149,14 +149,14 @@ public class Experience extends BaseEntity {
     }
 
     public void updateToStarForm(ExperienceUpdateRequestDto updateRequestDto) {
-        this.status = updateRequestDto.getStatus();
-        this.formType = updateRequestDto.getFormType();
+        this.status = Status.valueOf(updateRequestDto.getStatus());
+        this.formType = FormType.valueOf(updateRequestDto.getFormType());
         this.title = updateRequestDto.getTitle();
         this.isEnded = updateRequestDto.getEndDate().isBefore(LocalDate.now());
         this.startDate = updateRequestDto.getStartDate();
         this.endDate = updateRequestDto.getEndDate();
         this.keyword = updateRequestDto.getKeyword();
-        this.experienceType = updateRequestDto.getExperienceType();
+        this.experienceType = ExperienceType.valueOf(updateRequestDto.getExperienceType());
         this.situation = updateRequestDto.getSituation();
         this.task = updateRequestDto.getTask();
         this.action = updateRequestDto.getAction();
