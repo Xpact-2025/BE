f5df65042ea2cb75221b252e9eeff44e254f5e19
kite_U <yurry45@naver.com>
2025-04-06 19:18:33 +0900

    feature/#30 : 회원 마이페이지 조회 로직 구현

--- src/main/java/com/itstime/xpact/domain/member/controller/MemberController.java
+++ src/main/java/com/itstime/xpact/domain/member/controller/MemberController.java
@@ -1,10 +1,32 @@
 package com.itstime.xpact.domain.member.controller;
 
+import com.itstime.xpact.domain.member.service.MemberService;
+import com.itstime.xpact.global.response.RestResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    // 마이페이지 조회
+    @GetMapping
+    public ResponseEntity<RestResponse<?>> mypage(
+            @RequestHeader("Authorizatin") String authToken
+    ) {
+       String token = authToken.startsWith("Bearer ") ? authToken.substring(7) : authToken;
+       return ResponseEntity.ok(
+               RestResponse.ok(
+                       memberService.getMyinfo(token)
+               )
+       );
+    }
 
 }
--- src/main/java/com/itstime/xpact/domain/member/service/MemberService.java
+++ src/main/java/com/itstime/xpact/domain/member/service/MemberService.java
@@ -1,17 +1,51 @@
 package com.itstime.xpact.domain.member.service;
 
+import com.itstime.xpact.domain.member.dto.response.MemberInfoResponseDto;
 import com.itstime.xpact.domain.member.entity.Member;
 import com.itstime.xpact.domain.member.repository.MemberRepository;
+import com.itstime.xpact.global.auth.TokenProvider;
 import com.itstime.xpact.global.exception.CustomException;
 import com.itstime.xpact.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
+
+    // 마이페이지 조회하기
+    @Transactional(readOnly = true)
+    public MemberInfoResponseDto getMyinfo(String token) throws CustomException {
+
+        Member member = getMemberFromToken(token);
+
+        return member.toMemberInfoResponseDto(member);
+    }
+
+    // 회원 정보 수정하기
+    @Transactional
+    public MemberInfoResponseDto saveMyinfo(String token) throws CustomException {
+
+        Member member = getMemberFromToken(token);
+
+        return null;
+    }
+
+    // 학력에 대한 조회
+
+
+
+    // getMemberFromToke 메소드 생성
+    private Member getMemberFromToken(String token) throws CustomException {
+        Long memberId = tokenProvider.getMemberIdFromToken(token);
+
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> CustomException.of(ErrorCode.FAILED_JWT_INFO));
+    }
 
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId)
