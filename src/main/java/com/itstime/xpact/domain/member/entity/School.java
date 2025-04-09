ddbde62c55dfb5d596dabd8ba75fd7c6eb69030f
kite_U <yurry45@naver.com>
2025-04-09 14:15:06 +0900

    feature/#37 : School Entity 추가 ( 학교 이름, 학과 이름 저장 )

---
+++ src/main/java/com/itstime/xpact/domain/member/entity/School.java
@@ -0,0 +1,18 @@
+package com.itstime.xpact.domain.member.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "school")
+public class School {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "school_id", nullable = false)
+    private Long id;
+
+    @Column(name = "school_name")
+    private String schoolName;
+
+    @Column(name = "major")
+    private String major;
+}
