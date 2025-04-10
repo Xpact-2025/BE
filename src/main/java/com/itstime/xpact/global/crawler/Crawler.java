2dd67d940bdad556aa852c1fb6d0758e8ec4b9d9
Wonjae Lim <yyytir777@gmail.com>
2025-04-10 15:14:33 +0900

    fix/#45 : json읽어서 필요 자료형으로 반환

--- src/main/java/com/itstime/xpact/global/crawler/Crawler.java
+++ src/main/java/com/itstime/xpact/global/crawler/Crawler.java
@@ -1,26 +1,54 @@
 package com.itstime.xpact.global.crawler;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itstime.xpact.domain.recruit.dto.DetailRecruitDto;
+import com.itstime.xpact.global.exception.CustomException;
+import com.itstime.xpact.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class Crawler {
+
+    private static final String RECRUIT_JSON = "src/main/resources/static/recruit.json";
+    private static final String DETAIL_RECRUIT_JSON = "src/main/resources/static/detailRecruit.json";
+    private final ObjectMapper objectMapper;
 
     // 직무 데이터
     public List<String> getRecruits() {
-        return List.of("A", "B", "C");
+        List<String> recruits = new ArrayList<>();
+        try {
+            JsonNode jsonNodes = objectMapper.readTree(new File(RECRUIT_JSON));
+            for (JsonNode jsonNode : jsonNodes) {
+                recruits.add(jsonNode.get("recruit").asText());
+            }
+        } catch (IOException e) {
+            throw CustomException.of(ErrorCode.TEST);
+        }
+
+        return recruits;
     }
 
-    // 상세 직무 데이터 Map<String, String> => <직무, 상세직무>
     public List<DetailRecruitDto> getDetailRecruits() {
-        return List.of(DetailRecruitDto.of("A", "AA"),
-                DetailRecruitDto.of("A", "AB"),
-                DetailRecruitDto.of("B", "BA"),
-                DetailRecruitDto.of("B", "BC"),
-                DetailRecruitDto.of("C", "CC"),
-                DetailRecruitDto.of("C", "CD")
-                );
+        List<DetailRecruitDto> detailRecruits = new ArrayList<>();
+        try {
+            JsonNode jsonNodes = objectMapper.readTree(new File(DETAIL_RECRUIT_JSON));
+            for (JsonNode jsonNode : jsonNodes) {
+                DetailRecruitDto detailRecruitDto = objectMapper.treeToValue(jsonNode, DetailRecruitDto.class);
+                detailRecruits.add(detailRecruitDto);
+            }
+        } catch (IOException e) {
+            throw CustomException.of(ErrorCode.TEST);
+        }
+
+        return detailRecruits;
     }
 }
