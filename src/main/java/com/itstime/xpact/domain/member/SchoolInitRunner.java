ca0af7dd71353a09cfba8abe6a0ad026de1296d8
kite_U <yurry45@naver.com>
2025-04-11 12:05:34 +0900

    feature/#43 : SchoolInitRunner 클래스 생성 ( ApplicationReadyEvent 클래스 )

---
+++ src/main/java/com/itstime/xpact/domain/member/SchoolInitRunner.java
@@ -0,0 +1,34 @@
+package com.itstime.xpact.domain.member;
+
+import com.itstime.xpact.domain.member.repository.SchoolRepository;
+import com.itstime.xpact.domain.member.util.SchoolUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SchoolInitRunner {
+
+    private final SchoolUtil schoolUtil;
+    private final SchoolRepository schoolRepository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        try {
+            long schoolCount = schoolRepository.count();
+            if (schoolCount == 0 ) {
+                log.info("School 데이터가 비어있습니다. 파싱 시작...");
+                schoolUtil.syncAllSchools();
+                log.info("School 데이터 저장 완료");
+            } else {
+                log.info("School 데이터가 이미 존재합니다.");
+            }
+        } catch (Exception e) {
+            log.error("School 동기화 중 오류 발생");
+        }
+    }
+}
