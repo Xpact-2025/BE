d452323e0853d9be6b7987412e2335b023a87962
kite_U <yurry45@naver.com>
2025-04-11 12:01:30 +0900

    feature/#43 : XML WebClient Bean 추가 (학교 open api baseURL로 Bean 등록)

--- src/main/java/com/itstime/xpact/global/config/ApiConfig.java
+++ src/main/java/com/itstime/xpact/global/config/ApiConfig.java
@@ -1,15 +1,25 @@
 package com.itstime.xpact.global.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.Trie;
 import org.apache.commons.collections4.trie.PatriciaTrie;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.codec.xml.Jaxb2XmlDecoder;
+import org.springframework.http.codec.xml.Jaxb2XmlEncoder;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
+@Slf4j
 public class ApiConfig {
+
+    @Value("${external.api.school.base-url}")
+    private String baseUrl;
 
     // Trie Config
     @Bean
@@ -17,14 +27,35 @@ public class ApiConfig {
         return new PatriciaTrie<String>();
     }
 
+    // TODO : WebClient Bean 생성
     @Bean
-    public WebClient.Builder webClientBuilder() {
-
+    @Qualifier("xmlWebClient")
+    public WebClient xmlWebClient() {
         DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
         factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY);
 
-        return WebClient.builder()
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8");
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(clientCodecConfigurer -> {
+                    clientCodecConfigurer.defaultCodecs()
+                            .jaxb2Encoder(new Jaxb2XmlEncoder());
+                    clientCodecConfigurer.defaultCodecs()
+                            .jaxb2Decoder(new Jaxb2XmlDecoder());
+                })
+                .build();
 
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .filter(logRequest())
+                .uriBuilderFactory(factory)
+                .exchangeStrategies(exchangeStrategies)
+                .build();
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return (clientRequest, next) -> {
+            log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
+            clientRequest.headers().forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
+            return next.exchange(clientRequest);
+        };
     }
 }
