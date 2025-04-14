ac12816e132d4a38b61f51c42e7b2284fd4d2ce5
kite_U <yurry45@naver.com>
2025-04-14 09:41:24 +0900

    feature/#30 : 학력 정보 저장 및 반환 기능 구현

--- src/main/java/com/itstime/xpact/domain/member/service/SchoolService.java
+++ src/main/java/com/itstime/xpact/domain/member/service/SchoolService.java
@@ -1,25 +1,31 @@
 package com.itstime.xpact.domain.member.service;
 
-import com.itstime.xpact.domain.member.common.SchoolStatus;
 import com.itstime.xpact.domain.member.dto.request.SchoolSaveRequestDto;
+import com.itstime.xpact.domain.member.entity.Education;
+import com.itstime.xpact.domain.member.entity.Member;
+import com.itstime.xpact.domain.member.entity.School;
+import com.itstime.xpact.domain.member.repository.EducationRepository;
 import com.itstime.xpact.domain.member.repository.SchoolCustomRepositoryImpl;
 import com.itstime.xpact.domain.member.util.TrieUtil;
 import com.itstime.xpact.global.auth.SecurityProvider;
 import com.itstime.xpact.global.exception.CustomException;
 import com.itstime.xpact.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class SchoolService {
 
     private final SecurityProvider securityProvider;
     private final TrieUtil trieUtil;
     private final SchoolCustomRepositoryImpl schoolRepository;
+    private final EducationRepository educationRepository;
 
     @Transactional(readOnly = true)
     public List<String> autocompleteName(String term) throws CustomException {
@@ -51,24 +57,39 @@ public class SchoolService {
         return schoolRepository.findMajorBySchoolName(schoolName);
     }
 
+    @Transactional
     public String saveSchoolInfo(SchoolSaveRequestDto requestDto) throws CustomException {
 
-        try {
-            securityProvider.getCurrentMemberId();
+        Member member = securityProvider.getCurrentMember();
 
-            String education = requestDto.name() + " " + requestDto.major() + " ";
-            String status = "";
+        // 학교명과 학과명 입력
+        School school = schoolRepository.findBySchoolNameAndMajor(
+                    requestDto.name(), requestDto.major())
+                    .orElseGet(() -> saveNewSchool(requestDto));
 
-            if (requestDto.schoolStatus().equals(SchoolStatus.CURRENT)) {
-                status = "재학";
-            } else if (requestDto.schoolStatus().equals(SchoolStatus.GRADUATION)) {
-                status = "졸업";
-            } else if (requestDto.schoolStatus().equals(SchoolStatus.SUSPENDED)) {
-                status = "휴학";
-            }
-            return education + status;
-        } catch (Exception e) {
-            throw CustomException.of(ErrorCode.INVALID_SCHOOL_TYPE);
-        }
+        String educationName = createEducation(requestDto);
+
+        Education education = Education.builder()
+                    .member(member)
+                    .school(school)
+                    .schoolStatus(requestDto.schoolStatus())
+                    .educationName(educationName)
+                    .build();
+
+        member.setEducation(education); // 최종만 저장
+        educationRepository.save(education);
+        return educationName;
+    }
+
+    private School saveNewSchool(SchoolSaveRequestDto requestDto) throws CustomException {
+        log.info("{} {} : 새로운 학교 정보 저장 중...", requestDto.name(), requestDto.major());
+        return schoolRepository.saveIfNotExist(requestDto.name(), requestDto.major());
+    }
+
+    private String createEducation(SchoolSaveRequestDto requestDto) throws CustomException {
+        return String.format("%s %s %s",
+                requestDto.name(),
+                requestDto.major(),
+                requestDto.schoolStatus().getDisplayName());
     }
 }
