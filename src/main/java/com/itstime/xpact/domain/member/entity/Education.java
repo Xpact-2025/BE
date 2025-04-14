109dcb777495d48f145d862df0b48442d782653f
kite_U <yurry45@naver.com>
2025-04-14 09:35:54 +0900

    feature/#30 : Education Entity를 School(N:1)과 Member(1:1)에 매핑

--- src/main/java/com/itstime/xpact/domain/member/entity/Education.java
+++ src/main/java/com/itstime/xpact/domain/member/entity/Education.java
@@ -1,11 +1,39 @@
 package com.itstime.xpact.domain.member.entity;
 
+import com.itstime.xpact.domain.member.common.SchoolStatus;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "education")
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Education {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "school_id")
+    private School school;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "school_status")
+    @Enumerated(EnumType.STRING)
+    private SchoolStatus schoolStatus;
+
+    @Column(name ="is_verified")
+    private boolean isVerified;
+
+    @Column(name = "education_name")
+    private String educationName;
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
 }
