1d47b3db79c3da6c4fcb41a34dc008752fb77cce
kite_U <yurry45@naver.com>
2025-04-30 15:11:58 +0900

    feature/#30 : Education Entity와 DTO에 startedAt, endedAt 필드 추가 ( 입학날짜, 졸업날짜 )

--- src/main/java/com/itstime/xpact/domain/member/dto/request/MemberInfoRequestDto.java
+++ src/main/java/com/itstime/xpact/domain/member/dto/request/MemberInfoRequestDto.java
@@ -3,6 +3,8 @@ package com.itstime.xpact.domain.member.dto.request;
 import com.itstime.xpact.domain.recruit.entity.Recruit;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDate;
+
 @Schema
 public record MemberInfoRequestDto(
         @Schema(description = "회원 이름",
@@ -15,12 +17,20 @@ public record MemberInfoRequestDto(
         @Schema(description = "회원 나이")
         Integer age,
 
-        @Schema(description = "회원 학력 이름, Education에서 입력한 학력 정보의 String을 사용합니다.",
-                example = "잇타대학교 경영학과 재학")
+        @Schema(description = "회원 최종 학력 한 줄",
+                example = "잇타대학교 잇타학과 재학")
         String schoolInfo,
 
         @Schema(description = "희망 직무",
                 example = "회계사")
-        Recruit recruit
+        Recruit recruit,
+
+        @Schema(description = "입학 날짜",
+        example = "2025-03-02")
+        LocalDate startedAt,
+
+        @Schema(description = "졸업 날짜",
+        example = "2025-03-02")
+        LocalDate endedAt
 ) {
 }
--- src/main/java/com/itstime/xpact/domain/member/dto/response/MemberInfoResponseDto.java
+++ src/main/java/com/itstime/xpact/domain/member/dto/response/MemberInfoResponseDto.java
@@ -3,6 +3,8 @@ package com.itstime.xpact.domain.member.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+import java.time.LocalDate;
+
 @Builder
 @Schema(description = "회원 프로필 설정 DTO")
 public record MemberInfoResponseDto (
@@ -16,12 +18,20 @@ public record MemberInfoResponseDto (
         @Schema(description = "회원 나이")
         int age,
 
-        @Schema(description = "회원 학력 이름",
-        example = "잇타대학교 잇타학과 재학")
+        @Schema(description = "회원 최종 학력 한 줄",
+                example = "잇타대학교 잇타학과 재학")
         String education,
 
         @Schema(description = "희망 직무",
         example = "회계사")
-        String recruit
+        String recruit,
+
+        @Schema(description = "입학 날짜",
+                example = "2025-03-02")
+        LocalDate startedAt,
+
+        @Schema(description = "졸업 날짜",
+                example = "2025-03-02")
+        LocalDate endedAt
 ) {
 }
