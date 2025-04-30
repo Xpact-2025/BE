7bf7b26c8371f1f6b101abcbb00a6a2cb29bd15d
kite_U <yurry45@naver.com>
2025-04-30 18:56:40 +0900

    fix/#43: QueryDSL distinct 추가 ( 중복 문제 해결 )

--- src/main/java/com/itstime/xpact/domain/member/repository/EducationRepository.java
+++ src/main/java/com/itstime/xpact/domain/member/repository/EducationRepository.java
@@ -3,5 +3,9 @@ package com.itstime.xpact.domain.member.repository;
 import com.itstime.xpact.domain.member.entity.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EducationRepository extends JpaRepository<Education, Long> {
+
+    Optional<Education> findByMemberId(Long memberId);
 }
