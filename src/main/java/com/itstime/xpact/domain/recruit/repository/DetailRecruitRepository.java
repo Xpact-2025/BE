c38f631d58457d6863b73fe49ef41944cd804719
Wonjae Lim <yyytir777@gmail.com>
2025-04-30 15:15:15 +0900

    fix/#45 : detailrecruit, recruit간의 지연로딩으로 인한 fetch join 메서드 적용

--- src/main/java/com/itstime/xpact/domain/recruit/repository/DetailRecruitRepository.java
+++ src/main/java/com/itstime/xpact/domain/recruit/repository/DetailRecruitRepository.java
@@ -2,8 +2,14 @@ package com.itstime.xpact.domain.recruit.repository;
 
 import com.itstime.xpact.domain.recruit.entity.DetailRecruit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DetailRecruitRepository extends JpaRepository<DetailRecruit, Long> {
+
+    @Query("SELECT dr FROM DetailRecruit dr JOIN FETCH dr.recruit")
+    List<DetailRecruit> findAllWithRecruit();
 }
