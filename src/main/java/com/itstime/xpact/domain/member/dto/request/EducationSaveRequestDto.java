edf730e5051083c84930844a781a43f3a5512a84
kite_U <yurry45@naver.com>
2025-05-04 21:24:34 +0900

    chore/#55: 불필요한 패키지 삭제

--- src/main/java/com/itstime/xpact/domain/member/dto/request/EducationSaveRequestDto.java
+++ src/main/java/com/itstime/xpact/domain/member/dto/request/EducationSaveRequestDto.java
@@ -2,10 +2,6 @@ package com.itstime.xpact.domain.member.dto.request;
 
 import com.itstime.xpact.domain.member.common.SchoolStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 
