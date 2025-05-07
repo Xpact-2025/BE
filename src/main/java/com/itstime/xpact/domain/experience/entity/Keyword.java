b99474d48607e3e92234828cce80a61768d8bab6
Wonjae Lim <yyytir777@gmail.com>
2025-05-07 10:46:22 +0900

    fix/#53 : keyword 검증 메서드 생성

--- src/main/java/com/itstime/xpact/domain/experience/entity/Keyword.java
+++ src/main/java/com/itstime/xpact/domain/experience/entity/Keyword.java
@@ -1,11 +1,15 @@
 package com.itstime.xpact.domain.experience.entity;
 
 import com.itstime.xpact.domain.common.BaseEntity;
+import com.itstime.xpact.global.exception.CustomException;
+import com.itstime.xpact.global.exception.ErrorCode;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @Entity
@@ -20,10 +24,22 @@ public class Keyword extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", length = 10, nullable = false)
     private String name;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "experience_id")
     private Experience experience;
+
+    public static void validateKeyword(List<String> keywords) {
+        if(keywords.size() > 5) {
+            throw CustomException.of(ErrorCode.KEYWORD_EXCEEDED);
+        }
+
+        keywords.forEach(keyword -> {
+            if(keyword.length() > 10) {
+                throw CustomException.of(ErrorCode.KEYWORD_TOO_LONG);
+            }
+        });
+    }
 }
