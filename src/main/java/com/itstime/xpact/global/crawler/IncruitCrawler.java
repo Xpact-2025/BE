8e07b3c3f514c4c0f50008f2ec6a2007cf71e6ce
Wonjae Lim <yyytir777@gmail.com>
2025-05-07 23:33:02 +0900

    fix/#68 : 크롤러 주석 해제

--- src/main/java/com/itstime/xpact/global/crawler/IncruitCrawler.java
+++ src/main/java/com/itstime/xpact/global/crawler/IncruitCrawler.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Component
+@Component
 @RequiredArgsConstructor
 public class IncruitCrawler {
 
