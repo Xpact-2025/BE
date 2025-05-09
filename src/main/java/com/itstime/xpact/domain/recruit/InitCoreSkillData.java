528de7b7125c53c2fe96d65d509c9a7233ca693c
kite_U <yurry45@naver.com>
2025-05-09 15:13:12 +0900

    fix/#69: ApplicationRunner -> ApplicationReadyEvent로 변경 ( 데이터 파싱 오류 시 서버 장애 막기 )

--- src/main/java/com/itstime/xpact/domain/recruit/InitCoreSkillData.java
+++ src/main/java/com/itstime/xpact/domain/recruit/InitCoreSkillData.java
@@ -3,23 +3,28 @@ package com.itstime.xpact.domain.recruit;
 import com.itstime.xpact.domain.recruit.service.CoreSkillService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
-public class InitCoreSkillData implements ApplicationRunner {
+@Slf4j
+public class InitCoreSkillData {
 
     private final CoreSkillService coreSkillService;
 
-    public void run(ApplicationArguments args) {
-        if(!checkCoreSkillData()) coreSkillService.saveCoreSkillData();
-        else log.info("CoreSKill Column has already been initialized");
-    }
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        if (coreSkillService.existsCoreSkill()) {
+            log.info("CoreSKill Column has already been initialized");
+            return;
+        }
 
-    private boolean checkCoreSkillData() {
-        return coreSkillService.existsCoreSkill();
+        try {
+            coreSkillService.saveCoreSkillData();
+        } catch (Exception e) {
+            log.error("Error while initializing CoreSkill: {}", e.getMessage(), e);
+        }
     }
 }
