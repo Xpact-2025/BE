f9da4d518575bee04ed32ddf22d602b72356b4d4
kite_U <yurry45@naver.com>
2025-05-09 15:11:52 +0900

    feature/#69: findByName 메소드 구현

--- src/main/java/com/itstime/xpact/domain/recruit/repository/DetailRecruitRepository.java
+++ src/main/java/com/itstime/xpact/domain/recruit/repository/DetailRecruitRepository.java
@@ -7,15 +7,16 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface DetailRecruitRepository extends JpaRepository<DetailRecruit, Long> {
+public interface DetailRecruitRepository extends JpaRepository<DetailRecruit, Long>, DetailRecruitCustomRepository {
+
+    @Query("SELECT dr FROM DetailRecruit dr WHERE dr.name = :name")
+    Optional<DetailRecruit> findByName(@Param("name") String name);
 
     @Query("SELECT dr FROM DetailRecruit dr JOIN FETCH dr.recruit")
     List<DetailRecruit> findAllWithRecruit();
-
-    @Query("SELECT d.name FROM DetailRecruit d WHERE d.recruit.id = :recruitId")
-    List<String> findAllByRecruitId(@Param("recruitId") Long recruitId);
 
     @Query("SELECT d.name FROM DetailRecruit d WHERE d.recruit.id = :recruitId")
     List<String> findDetailRecruitNamesByRecruitId(@Param("recruitId") Long recruitId);
