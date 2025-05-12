aaf1123487c5d8a8957224cbdc79ba39b86ce514
Wonjae Lim <yyytir777@gmail.com>
2025-05-12 16:27:56 +0900

    feature/#64 : memberId로 각각 DetailRecruit, Keyword 페치조인하는 메서드 생성

--- src/main/java/com/itstime/xpact/domain/experience/repository/ExperienceRepository.java
+++ src/main/java/com/itstime/xpact/domain/experience/repository/ExperienceRepository.java
@@ -1,18 +1,24 @@
 package com.itstime.xpact.domain.experience.repository;
 
-import com.itstime.xpact.domain.experience.common.ExperienceType;
 import com.itstime.xpact.domain.experience.entity.Experience;
 import com.itstime.xpact.domain.member.entity.Member;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface ExperienceRepository extends JpaRepository<Experience, Long>, ExperienceCustomRepository {
 
     List<Experience> findAllByMember(Member member, Sort sort);
+
+    @Query("SELECT e FROM Experience e JOIN FETCH e.detailRecruit WHERE e.member.id = :memberId")
+    List<Experience> findAllWithDetailRecruitByMemberId(@Param("memberId") Long memberId);
+
+    @Query("SELECT e FROM Experience e JOIN FETCH e.keywords WHERE e.member.id = :memberId")
+    List<Experience> findAllWithKeywordByMemberId(@Param("memberId") Long memberId);
 
 }
