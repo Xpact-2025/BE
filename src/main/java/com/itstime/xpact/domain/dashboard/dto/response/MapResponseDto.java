cb104852b1693ff228dd88abcf664ea7bbedc215
kite_U <yurry45@naver.com>
2025-05-12 15:42:40 +0900

    fix/#61: Data annotation 추가(직렬화 가능하도록 추가)

--- src/main/java/com/itstime/xpact/domain/dashboard/dto/response/MapResponseDto.java
+++ src/main/java/com/itstime/xpact/domain/dashboard/dto/response/MapResponseDto.java
@@ -1,6 +1,7 @@
 package com.itstime.xpact.domain.dashboard.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Data
 public class MapResponseDto {
     private List<ScoreResponseDto> coreSkillMaps;
 
--- src/main/java/com/itstime/xpact/domain/dashboard/dto/response/ScoreResponseDto.java
+++ src/main/java/com/itstime/xpact/domain/dashboard/dto/response/ScoreResponseDto.java
@@ -1,12 +1,14 @@
 package com.itstime.xpact.domain.dashboard.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
 @AllArgsConstructor
+@Data
 public class ScoreResponseDto {
 
     private String coreSkillName;
