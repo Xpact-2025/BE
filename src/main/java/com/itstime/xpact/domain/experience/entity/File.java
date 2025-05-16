88d3cc22301eb59a9cbc905904f5508be81b8ea2
Wonjae Lim <yyytir777@gmail.com>
2025-05-16 17:00:29 +0900

    feature/#76 : Experience와 1대다 매핑되는 File 엔티티 생성

---
+++ src/main/java/com/itstime/xpact/domain/experience/entity/File.java
@@ -0,0 +1,28 @@
+package com.itstime.xpact.domain.experience.entity;
+
+import com.itstime.xpact.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@Table(name = "file")
+@NoArgsConstructor
+@AllArgsConstructor
+public class File extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "file_url")
+    private String fileUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "experience_id")
+    private Experience experience;
+}
