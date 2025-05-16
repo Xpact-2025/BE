b66bb0e519db8e38c4d733b58731f1c75da3b4a7
Wonjae Lim <yyytir777@gmail.com>
2025-05-16 17:01:45 +0900

    fix/#76 : CreateRequestDto 수정 (필드간의 순서 변경 및 새로운 필드 추가)

--- src/main/java/com/itstime/xpact/domain/experience/dto/request/ExperienceUpdateRequestDto.java
+++ src/main/java/com/itstime/xpact/domain/experience/dto/request/ExperienceUpdateRequestDto.java
@@ -14,48 +14,61 @@ import java.util.List;
 public class ExperienceUpdateRequestDto {
 
     // common (공통 부분)
-    @Schema(description = "경험 저장 방식 지정 (임시저장 or 저장)", example = "SAVE", allowableValues = {"STASH", "SAVE"})
+    @Schema(description = "경험 저장 방식 지정 (임시저장 or 저장)", example = "SAVE", allowableValues = {"SAVE", "DRAFT"})
     private String status;
-
+    @Schema(description = "경험 양식 지정 (Star양식 or 간결 양식)", example = "STAR_FORM")
+    private String formType;
     @Schema(description = "경험 유형 지정",
-            example = "PROJECT",
+            example = "INTERN",
             allowableValues = {"INTERN", "EXTERNAL_ACTIVITIES", "CONTEST",
                     "PROJECT", "CERTIFICATES", "ACADEMIC_CLUB", "EDUCATION",
-                    "VOLUNTEER_WORK", "STUDY_ABROAD", "ETC"})
+                    "PRIZE", "VOLUNTEER_WORK", "STUDY_ABROAD", "ETC"})
     private String experienceType;
 
-    @Schema(description = "경험 양식 지정 (Star양식 or 간결 양식", example = "SIMPLE_FORM")
-    private String formType;
-
-    @Schema(description = "경험 제목", example = "경험 제목")
-    private String title;
 
     @Schema(description = "시작 일시", example = "2025-03-27")
     private LocalDate startDate;
-
     @Schema(description = "종료 일시", example = "2025-03-27")
     private LocalDate endDate;
 
-    @Schema(description = "키워드")
-    private List<String> keywords;
+
+    @Schema(description = "경험 제목", example = "제목을 입력하세요")
+    private String title;
+
+
+    @Schema(description = "증명서", example = "파일 링크")
+    private String warrant;
+    @Schema(description = "산출물", example = "[\"파일 링크\"]")
+    private List<String> files;
+
 
     // STAR_FORM
     @Schema(description = "상황", example = "상황을 입력하세요")
     private String situation;
-
     @Schema(description = "문제", example = "문제를 입력하세요")
     private String task;
-
     @Schema(description = "해결방법", example = "해결방법을 입력하세요")
     private String action;
-
     @Schema(description = "결과", example = "결과를 입력하세요")
     private String result;
 
     // SIMPLE_FORM
     @Schema(description = "역할", example = "역할을 입력하세요")
     private String role;
-
     @Schema(description = "주요성과", example = "주요성과를 입력하세요")
     private String perform;
+
+
+    @Schema(description = "자격증명/수상명", example = "자격증명/수상명을 입력하세요")
+    private String qualification;
+    @Schema(description = "발행처/대회명", example = "발행처/대회명을 입력하세요")
+    private String publisher;
+    @Schema(description = "발행일/수상일", example = "발행일/수상일을 입력하세요")
+    private LocalDate issueDate;
+    @Schema(description = "간단설명", example = "간단설명을 입력하세요")
+    private String simpleDescription;
+
+
+    @Schema(description = "키워드", example = "[\"파일 링크\"]")
+    private List<String> keywords;
 }
