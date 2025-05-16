511b42ec29a3e874bcef6694b44fe8a252364a78
kite_U <yurry45@naver.com>
2025-05-16 13:13:02 +0900

    fix/#87: JOIN FETCH를 통한 LazyInitializationException 해결

--- src/main/java/com/itstime/xpact/domain/member/repository/MemberRepository.java
+++ src/main/java/com/itstime/xpact/domain/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.itstime.xpact.domain.member.repository;
 
 import com.itstime.xpact.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByEmail(String email);
     Optional<Member> findByEmail(String email);
+
+    @Query("SELECT m FROM Member m JOIN FETCH m.experiences WHERE m.id = :id")
+    Optional<Member> findByIdWithExperiences(@Param("id") Long memberId);
 }
