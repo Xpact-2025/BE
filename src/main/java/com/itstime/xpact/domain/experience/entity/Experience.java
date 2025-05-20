3af8c5ddc3a188386997cd41ca0723ec75502a8c
Wonjae Lim <yyytir777@gmail.com>
2025-05-21 01:03:59 +0900

    fix : qualification 경험 객체는 endDate 없으므로 issueDate로 변경

--- src/main/java/com/itstime/xpact/domain/experience/entity/Experience.java
+++ src/main/java/com/itstime/xpact/domain/experience/entity/Experience.java
@@ -133,7 +133,7 @@ public class Experience extends BaseEntity {
                         .status(Status.valueOf(createRequestDto.getStatus()))
                         .formType(FormType.valueOf(createRequestDto.getFormType()))
                         .experienceType(ExperienceType.valueOf(createRequestDto.getExperienceType())).build())
-                .isEnded(createRequestDto.getEndDate().isBefore(LocalDate.now()))
+                .isEnded(createRequestDto.getIssueDate().isBefore(LocalDate.now()))
                 .startDate(createRequestDto.getStartDate())
                 .endDate(createRequestDto.getEndDate())
                 .title(null)
