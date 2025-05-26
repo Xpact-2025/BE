1fbc073f6f439f0b7ab89525b782da834e20baf8
kite_U <yurry45@naver.com>
2025-05-26 12:21:40 +0900

    feature/#121: Redis에 저장할 CoreSkillMap 생성

---
+++ src/main/java/com/itstime/xpact/domain/dashboard/repository/CoreSkillMapRepository.java
@@ -0,0 +1,7 @@
+package com.itstime.xpact.domain.dashboard.repository;
+
+import com.itstime.xpact.domain.dashboard.entity.CoreSkillMap;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CoreSkillMapRepository extends CrudRepository<CoreSkillMap, Long> {
+}
