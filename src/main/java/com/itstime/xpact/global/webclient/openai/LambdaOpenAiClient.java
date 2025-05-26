a5bfa8eb18e57a550e080a32b48bbfb123618065
kite_U <yurry45@naver.com>
2025-05-26 12:18:57 +0900

    feature/#121: LambdaFunction에 요청을 보내는 WebClient 생성

---
+++ src/main/java/com/itstime/xpact/global/webclient/openai/LambdaOpenAiClient.java
@@ -0,0 +1,33 @@
+package com.itstime.xpact.global.webclient.openai;
+
+import com.itstime.xpact.domain.dashboard.dto.response.SkillMapResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class LambdaOpenAiClient {
+
+    private final WebClient lambdaWebClient;
+
+    @Async
+    public CompletableFuture<SkillMapResponseDto> requestEvaluation(OpenAiRequestDto requestDto) {
+        SkillMapResponseDto resultDto = lambdaWebClient
+                .post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/skillmaps")
+                        .build())
+                .bodyValue(requestDto)
+                .retrieve()
+                .bodyToMono(SkillMapResponseDto.class)
+                .block();
+
+        return CompletableFuture.completedFuture(resultDto);
+    }
+}
