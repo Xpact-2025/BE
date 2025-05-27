740b90cc2be94a60e0bdf2063cbb91604a939283
Wonjae Lim <yyytir777@gmail.com>
2025-05-28 01:43:55 +0900

    fix/#120 : Experience 엔티티, dto 간 변환 클래스 수정

--- src/main/java/com/itstime/xpact/domain/experience/converter/ExperienceConverter.java
+++ src/main/java/com/itstime/xpact/domain/experience/converter/ExperienceConverter.java
@@ -8,137 +8,137 @@ import com.itstime.xpact.domain.experience.dto.request.ExperienceUpdateRequestDt
 import com.itstime.xpact.domain.experience.entity.Experience;
 import com.itstime.xpact.domain.experience.entity.File;
 import com.itstime.xpact.domain.experience.entity.Keyword;
+import com.itstime.xpact.domain.experience.entity.SubExperience;
 import com.itstime.xpact.domain.experience.entity.embeddable.SimpleForm;
 import com.itstime.xpact.domain.experience.entity.embeddable.StarForm;
-import com.itstime.xpact.global.exception.ErrorCode;
-import com.itstime.xpact.global.exception.GeneralException;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.itstime.xpact.domain.experience.common.ExperienceType.IS_QUALIFICATION;
 
 @Component
 public class ExperienceConverter {
 
-    public List<Experience> toEntity(ExperienceCreateRequestDto createRequestDto) {
-        return createRequestDto.getSubExps().stream()
-                .map(subExperience -> {
-                    // 공통 필드 처리
-                    Experience.ExperienceBuilder experienceBuilder = commonFieldsBuilder(createRequestDto);
-                    // 나머지 필드 처리
-                    Experience experience = subFieldsBuilder(experienceBuilder, subExperience, ExperienceType.valueOf(createRequestDto.getExperienceType()));
-                    // 각 sub경험마다 keywords와 files 매핑
-                    setKeywordsAndFiles(experience, subExperience);
-                    return experience;
-                })
-                .toList();
+    public Experience createExperience(ExperienceCreateRequestDto createRequestDto) {
+        Experience experience = commonFieldsBuilder(createRequestDto);
+
+        List<SubExperience> subExperiences = createRequestDto.getSubExperiences().stream()
+                .map(subExperienceDto -> {
+                // 나머지 subExperiences 생성
+                SubExperience subExperience = subFieldsBuilder(subExperienceDto, ExperienceType.valueOf(createRequestDto.getExperienceType()));
+
+                if(!IS_QUALIFICATION.contains(experience.getExperienceType())) {
+                    setFiles(subExperience, subExperienceDto.getFiles());
+                }
+                setKeywords(subExperience, subExperienceDto.getKeywords());
+
+                return subExperience;
+            }).toList();
+
+        setMapping(experience, subExperiences);
+        return experience;
+    }
+
+    private void setMapping(Experience experience, List<SubExperience> subExperiences) {
+        experience.getSubExperiences().clear();
+        experience.getSubExperiences().addAll(subExperiences);
+        subExperiences.forEach(subExperience -> subExperience.setExperience(experience));
     }
 
     /**
      * metaData, title, startDate, endDate로만 experience 엔티티 생성
      * 후에 나머지 값들은 subFieldsBuilder에서 builder처리
      */
-    private Experience.ExperienceBuilder commonFieldsBuilder(ExperienceCreateRequestDto createRequestDto) {
+    private Experience commonFieldsBuilder(ExperienceCreateRequestDto createRequestDto) {
         // 수상 및 자격증 경험일 때
         if(IS_QUALIFICATION.contains(ExperienceType.valueOf(createRequestDto.getExperienceType()))) {
             return Experience.builder()
+                    .status(Status.valueOf(createRequestDto.getStatus()))
                     .experienceType(ExperienceType.valueOf(createRequestDto.getExperienceType()))
                     .qualification(createRequestDto.getQualification())
                     .publisher(createRequestDto.getPublisher())
                     .startDate(createRequestDto.getStartDate())
                     .endDate(createRequestDto.getEndDate())
-                    .isEnded(createRequestDto.getEndDate().isBefore(LocalDate.now()));
+                    .isEnded(createRequestDto.getEndDate().isBefore(LocalDate.now()))
+                    .build();
         } else {
             return Experience.builder()
+                    .status(Status.valueOf(createRequestDto.getStatus()))
                     .experienceType(ExperienceType.valueOf(createRequestDto.getExperienceType()))
                     .title(createRequestDto.getTitle())
                     .startDate(createRequestDto.getStartDate())
                     .endDate(createRequestDto.getEndDate())
-                    .isEnded(createRequestDto.getEndDate().isBefore(LocalDate.now()));
+                    .isEnded(createRequestDto.getEndDate().isBefore(LocalDate.now()))
+                    .build();
         }
     }
 
-    private Experience subFieldsBuilder(Experience.ExperienceBuilder experienceBuilder, ExperienceCreateRequestDto.SubExperience subExperience, ExperienceType experienceType) {
-        Experience experience = null;
+    private SubExperience subFieldsBuilder(ExperienceCreateRequestDto.SubExperienceRequestDto subExperienceDto, ExperienceType experienceType) {
+        SubExperience subExperience = null;
         // 수상 및 자격증 경험일 때
         if(IS_QUALIFICATION.contains(experienceType)) {
-            experience = experienceBuilder
-                    .subTitle(subExperience.getSubTitle())
-                    .simpleDescription(subExperience.getSimpleDescription())
-                    .status(Status.valueOf(subExperience.getStatus()))
-                    .formType(FormType.STAR_FORM)
+            subExperience = SubExperience.builder()
+                    .subTitle(subExperienceDto.getSubTitle())
+                    .simpleDescription(subExperienceDto.getSimpleDescription())
+                    .formType(FormType.valueOf(subExperienceDto.getFormType()))
                     .build();
         } else {
-            switch (FormType.valueOf(subExperience.getFormType())) {
-                case STAR_FORM -> experience = experienceBuilder
-                            .subTitle(subExperience.getSubTitle())
-                            .status(Status.valueOf(subExperience.getStatus()))
+            switch (FormType.valueOf(subExperienceDto.getFormType())) {
+                case STAR_FORM -> subExperience = SubExperience.builder()
+                            .subTitle(subExperienceDto.getSubTitle())
                             .formType(FormType.STAR_FORM)
                             .starForm(StarForm.builder()
-                                    .situation(subExperience.getSituation())
-                                    .task(subExperience.getTask())
-                                    .action(subExperience.getAction())
-                                    .result(subExperience.getResult())
+                                    .situation(subExperienceDto.getSituation())
+                                    .task(subExperienceDto.getTask())
+                                    .action(subExperienceDto.getAction())
+                                    .result(subExperienceDto.getResult())
                                     .build())
                             .build();
-                case SIMPLE_FORM -> experience = experienceBuilder
-                            .subTitle(subExperience.getSubTitle())
-                            .status(Status.valueOf(subExperience.getStatus()))
+                case SIMPLE_FORM -> subExperience = SubExperience.builder()
+                            .subTitle(subExperienceDto.getSubTitle())
                             .formType(FormType.SIMPLE_FORM)
                             .simpleForm(SimpleForm.builder()
-                                    .role(subExperience.getRole())
-                                    .perform(subExperience.getPerform())
+                                    .role(subExperienceDto.getRole())
+                                    .perform(subExperienceDto.getPerform())
                                     .build())
                             .build();
             }
         }
-        return experience;
+        return subExperience;
     }
 
-    private void setKeywordsAndFiles(Experience experience, ExperienceCreateRequestDto.SubExperience subExperience) {
-        experience.setKeywords(subExperience.getKeywords().stream()
-                .map(str -> Keyword.builder()
-                            .name(str)
-                            .experience(experience)
-                            .build()).toList());
+    public SubExperience updateSubExperience(Map<Long, SubExperience> subExperienceMap, ExperienceUpdateRequestDto updateRequestDto, ExperienceUpdateRequestDto.SubExperienceRequestDto subExperienceRequestDto) {
+        SubExperience subExperience = subExperienceMap.getOrDefault(subExperienceRequestDto.getSubExperienceId(), new SubExperience());
+        subExperience.updateSubFields(subExperienceRequestDto, ExperienceType.valueOf(updateRequestDto.getExperienceType()));
 
-        experience.setFiles(subExperience.getFiles().stream()
+        if(!IS_QUALIFICATION.contains(ExperienceType.valueOf(updateRequestDto.getExperienceType()))) {
+            setFiles(subExperience, subExperienceRequestDto.getFiles());
+        }
+        setKeywords(subExperience, subExperienceRequestDto.getKeywords());
+
+        return subExperience;
+    }
+
+    private void setFiles(SubExperience subExperience, List<String> files) {
+        subExperience.getFiles().clear();
+        subExperience.setFiles(files.stream()
                 .map(url -> File.builder()
                         .fileUrl(url)
-                        .experience(experience)
+                        .subExperience(subExperience)
                         .build()).toList());
     }
 
-    public Experience updateEntity(Map<Long, Experience> existingExperiences, ExperienceUpdateRequestDto updateRequestDto, ExperienceUpdateRequestDto.SubExperience targetExperience) {
-        Experience experience = existingExperiences.getOrDefault(targetExperience.getSubExperienceId(), new Experience());
-        experience.updateCommonFields(updateRequestDto);
-
-        experience.updateSubFields(targetExperience, ExperienceType.valueOf(updateRequestDto.getExperienceType()));
-        setKeywordsAndFiles(experience, targetExperience);
-
-        if(Status.SAVE.equals(experience.getStatus()) && Status.DRAFT.equals(Status.valueOf(targetExperience.getStatus()))){
-            throw GeneralException.of(ErrorCode.INVALID_SAVE);
-        }
-
-        return experience;
-    }
-
-    private void setKeywordsAndFiles(Experience experience, ExperienceUpdateRequestDto.SubExperience subExperience) {
-        experience.getKeywords().clear();
-        experience.setKeywords(subExperience.getKeywords().stream()
+    private void setKeywords(SubExperience subExperience, List<String> keywords) {
+        subExperience.getKeywords().clear();
+        subExperience.setKeywords(keywords.stream()
                 .map(str -> Keyword.builder()
                         .name(str)
-                        .experience(experience)
+                        .subExperience(subExperience)
                         .build()).toList());
 
-        experience.getFiles().clear();
-        experience.setFiles(subExperience.getFiles().stream()
-                .map(url -> File.builder()
-                        .fileUrl(url)
-                        .experience(experience)
-                        .build()).toList());
     }
 }
