1da38601ad0b48b1e7ce558928267077ec6f2831
Wonjae Lim <yyytir777@gmail.com>
2025-05-27 12:52:40 +0900

    fix/#120 : 임시저장 시 시간까지 나타낼 수 있도록 수정

--- src/main/java/com/itstime/xpact/domain/experience/dto/response/ThumbnailExperienceReadResponseDto.java
+++ src/main/java/com/itstime/xpact/domain/experience/dto/response/ThumbnailExperienceReadResponseDto.java
@@ -7,6 +7,8 @@ import com.itstime.xpact.domain.experience.entity.Experience;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -14,14 +16,16 @@ public class ThumbnailExperienceReadResponseDto {
     private Long id;
     private String title;
     private ExperienceType experienceType;
+    private LocalDateTime draftTime;
     private Status status;
 
     public static ThumbnailExperienceReadResponseDto of(Experience experience) {
         return ThumbnailExperienceReadResponseDto.builder()
                 .id(experience.getId())
-                .title(experience.getTitle() != null ? experience.getTitle() : experience.getQualification().getQualification())
-                .experienceType(experience.getMetaData().getExperienceType())
-                .status(experience.getMetaData().getStatus())
+                .title(experience.getTitle() != null ? experience.getTitle() : experience.getQualification())
+                .experienceType(experience.getExperienceType())
+                .draftTime(experience.getStatus().equals(Status.DRAFT) ? experience.getModifiedTime() : null)
+                .status(experience.getStatus())
                 .build();
     }
 }
