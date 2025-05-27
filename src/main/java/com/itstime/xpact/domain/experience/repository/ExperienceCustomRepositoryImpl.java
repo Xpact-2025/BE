5509d6462afa1f22ddcc132c04a589e8815e46fd
Wonjae Lim <yyytir777@gmail.com>
2025-05-28 01:43:00 +0900

    fix/#120 : 쿼리 로직 수정

--- src/main/java/com/itstime/xpact/domain/experience/repository/ExperienceCustomRepositoryImpl.java
+++ src/main/java/com/itstime/xpact/domain/experience/repository/ExperienceCustomRepositoryImpl.java
@@ -5,11 +5,11 @@ import com.itstime.xpact.domain.experience.common.Status;
 import com.itstime.xpact.domain.experience.entity.Experience;
 import com.itstime.xpact.domain.experience.entity.QExperience;
 import com.itstime.xpact.domain.experience.entity.QKeyword;
+import com.itstime.xpact.domain.experience.entity.QSubExperience;
 import com.itstime.xpact.domain.member.entity.Member;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
@@ -27,13 +27,7 @@ public class ExperienceCustomRepositoryImpl implements ExperienceCustomRepositor
         QExperience experience = QExperience.experience;
 
         return queryFactory.selectFrom(experience)
-                .where(experience.id.in(
-                        JPAExpressions
-                                .select(experience.id.min())
-                                .from(experience)
-                                .where(experience.groupExperience.member.eq(member))
-                                .groupBy(experience.groupExperience.id)
-                                .limit(1)))
+                .where(experience.member.id.eq(member.getId()))
                 .fetch();
     }
 
@@ -52,14 +46,8 @@ public class ExperienceCustomRepositoryImpl implements ExperienceCustomRepositor
             builder.or(experience.experienceType.eq(type));
         }
 
-        List<Long> ids = queryFactory.select(experience.id.min())
-                .from(experience)
-                .where(experience.groupExperience.member.eq(member).and(builder))
-                .groupBy(experience.groupExperience.id)
-                .fetch();
-
         return queryFactory.selectFrom(experience)
-                .where(experience.id.in(ids))
+                .where(builder.and(experience.member.id.eq(member.getId())))
                 .orderBy(orderSpecifier)
                 .fetch();
     }
@@ -76,6 +64,7 @@ public class ExperienceCustomRepositoryImpl implements ExperienceCustomRepositor
 
     public List<Experience> queryExperience(Member member, String query) {
         QExperience experience = QExperience.experience;
+        QSubExperience subExperience = QSubExperience.subExperience;
         QKeyword keyword = QKeyword.keyword;
 
         BooleanBuilder condition = new BooleanBuilder();
@@ -83,20 +72,16 @@ public class ExperienceCustomRepositoryImpl implements ExperienceCustomRepositor
         if(query != null) {
             condition.and(
                     experience.title.containsIgnoreCase(query)
-                            .or(keyword.name.containsIgnoreCase(query))
+                        .or(experience.qualification.containsIgnoreCase(query))
+                        .or(keyword.name.containsIgnoreCase(query))
             );
         }
 
-        List<Long> ids = queryFactory.select(experience.id.min())
-                .from(experience)
-                .leftJoin(experience.keywords, keyword)
-                .where(condition.and(experience.groupExperience.member.eq(member)))
-                .groupBy(experience.groupExperience.id)
-                .fetch();
-
         return queryFactory.selectFrom(experience)
-                .leftJoin(experience.keywords, keyword).fetchJoin()
-                .where(experience.id.in(ids))
+                .leftJoin(experience.subExperiences, subExperience)
+                .leftJoin(subExperience.keywords, keyword)
+                .where(condition.and(experience.member.id.eq(member.getId())))
+                .distinct()
                 .fetch();
     }
 }
