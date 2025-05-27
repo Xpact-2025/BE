cb3880a1373037d9bc285ef7a602a2c128b1ad0b
Wonjae Lim <yyytir777@gmail.com>
2025-05-27 12:53:29 +0900

    fix/#120 : ERD변경으로 인한 쿼리 수정

--- src/main/java/com/itstime/xpact/domain/experience/repository/ExperienceRepository.java
+++ src/main/java/com/itstime/xpact/domain/experience/repository/ExperienceRepository.java
@@ -1,9 +1,11 @@
 package com.itstime.xpact.domain.experience.repository;
 
 import com.itstime.xpact.domain.experience.entity.Experience;
+import com.itstime.xpact.domain.experience.entity.GroupExperience;
 import com.itstime.xpact.domain.member.entity.Member;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,27 +16,30 @@ import java.util.List;
 @Repository
 public interface ExperienceRepository extends JpaRepository<Experience, Long>, ExperienceCustomRepository {
 
-    List<Experience> findAllByMember(Member member, Sort sort);
-
-    @Query("SELECT e FROM Experience e JOIN FETCH e.detailRecruit WHERE e.member.id = :memberId AND e.detailRecruit is not null ")
+    @Query("SELECT e FROM Experience e JOIN FETCH e.detailRecruit WHERE e.groupExperience.member.id = :memberId AND e.detailRecruit is not null ")
     List<Experience> findAllWithDetailRecruitByMemberId(@Param("memberId") Long memberId);
 
-    @Query("SELECT e FROM Experience e JOIN FETCH e.keywords WHERE e.member.id = :memberId")
+    @Query("SELECT e FROM Experience e JOIN FETCH e.keywords WHERE e.groupExperience.member.id = :memberId")
     List<Experience> findAllWithKeywordByMemberId(@Param("memberId") Long memberId);
 
     @Query("SELECT FUNCTION('DATE_FORMAT', e.createdTime, '%Y-%m-%d') AS DATE, count(*) " +
             "FROM Experience e " +
-            "WHERE e.createdTime >= :startDate AND e.createdTime < :endDate AND e.member = :member " +
+            "WHERE e.createdTime >= :startDate AND e.createdTime < :endDate AND e.groupExperience.member = :member " +
             "GROUP BY DATE ")
     List<Object[]> countOldByDay(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, Member member);
 
     @Query("SELECT FUNCTION('DATE_FORMAT', e.createdTime, '%Y-%m-%d') AS DATE, count(*) " +
             "FROM Experience e " +
-            "WHERE e.createdTime >= :startDate AND e.createdTime < :endDate AND e.member = :member " +
+            "WHERE e.createdTime >= :startDate AND e.createdTime < :endDate AND e.groupExperience.member = :member " +
             "GROUP BY DATE ")
     List<Object[]> countByDay(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, Member member);
 
+    @Modifying
+    @Query("DELETE FROM Experience e WHERE e.groupExperience.member = :member ")
     void deleteAllByMember(Member member);
 
+    @Query("SELECT e FROM Experience e WHERE e.id IN :subExperienceIds ")
+    List<Experience> findByIds(List<Long> subExperienceIds);
 
+    List<Experience> findByGroupExperience(GroupExperience groupExperience);
 }
