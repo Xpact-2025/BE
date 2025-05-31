222fce7c832995eb0cf3a73bb46e45ef74ea5aab
kite_U <yurry45@naver.com>
2025-05-31 11:25:39 +0900

    fix/#131: 핵심스킬맵 응답 필드 오타 수정(JSON 파싱 불가 문제 해결)

--- src/main/java/com/itstime/xpact/domain/dashboard/dto/response/StrengthFeedback.java
+++ src/main/java/com/itstime/xpact/domain/dashboard/dto/response/StrengthFeedback.java
@@ -6,7 +6,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class StrengthFeedback {
-    private String weaknessName;
+    @Schema(name = "핵심 역량 중 강점 역량의 이름", example = "프로젝트 관리")
+    private String strengthName;
+    @Schema(name = "강점 분석 이유", example = "유저")
     private String reason;
     private String careerSuggestion;
 }
--- src/main/java/com/itstime/xpact/domain/dashboard/dto/response/WeaknessFeedback.java
+++ src/main/java/com/itstime/xpact/domain/dashboard/dto/response/WeaknessFeedback.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class WeaknessFeedback {
-    private String strengthName;
+    private String weaknessName;
     private String reason;
     private String improvementSuggestion;
 }
