06d71b80d1fb4b734f7a5e8d6e360dd08b1990df
kite_U <yurry45@naver.com>
2025-06-02 10:55:04 +0900

    feature/#134: Controller에 Swagger 추가

--- src/main/java/com/itstime/xpact/domain/guide/controller/GuideController.java
+++ src/main/java/com/itstime/xpact/domain/guide/controller/GuideController.java
@@ -2,6 +2,8 @@ package com.itstime.xpact.domain.guide.controller;
 
 import com.itstime.xpact.domain.guide.service.GuideService;
 import com.itstime.xpact.global.response.RestResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "가이드 API Controller")
 @RequestMapping("/api/guide")
 public class GuideController {
 
     private final GuideService guideService;
 
+    @Operation(summary = "약점에 대하여 분석을 요청 API", description = """
+            약점에 대하여 새로운 분석을 원할 때 요청할 수 있는 API입니다.<br>
+            약점의 디테일한 분석을 처음해보거나,<br>
+            경험을 갱신하고자 할 때 해당 요청을 보내주세요.
+            """)
     @PostMapping("/weakness")
     public ResponseEntity<RestResponse<?>> analysis() {
         guideService.saveWeakness();
@@ -24,6 +32,11 @@ public class GuideController {
         );
     }
 
+    @Operation(summary = "약점에 대한 분석 조회 API", description = """
+            약점에 대한 분석을 조회하고자 할 때 사용하는 API입니다.<br>
+            만약 대시보드에서 핵심스킬맵 산정이 안 되어있다면,<br>
+            이를 우선으로 실행 뒤에 요청해주세요.
+            """)
     @GetMapping("/weakness")
     public ResponseEntity<RestResponse<?>> getWeakness() {
         return ResponseEntity.ok(
