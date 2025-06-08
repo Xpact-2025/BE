923a0b3acc8b71ddbc3f8a17f8b0eeb5e9f442bc
kite_U <yurry45@naver.com>
2025-06-08 14:53:34 +0900

    feature/#142: Scrap Entity 필드 추가 및 연관관계 삭제 (독립적인 데이터 보관 테이블로 변경)

--- src/main/java/com/itstime/xpact/domain/guide/entity/Scrap.java
+++ src/main/java/com/itstime/xpact/domain/guide/entity/Scrap.java
@@ -1,19 +1,42 @@
 package com.itstime.xpact.domain.guide.entity;
 
 import com.itstime.xpact.domain.common.BaseEntity;
-import com.itstime.xpact.domain.member.entity.Member;
+import com.itstime.xpact.domain.guide.common.ScrapType;
 import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "scrap")
+@Getter
 public class Scrap extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "scrap_id")
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ScrapType scrapType;
 
     private String title;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+    private String speculation;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    private String detailRecruit;
+
+    private String coreSkill;
+
+    private String enterprise;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    private String region;
 }
