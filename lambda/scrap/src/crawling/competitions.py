beca6786e8e8890df100c895ba4aa7685ed2de2c
Wonjae Lim <yyytir777@gmail.com>
2025-06-25 13:31:23 +0900

    feature/#149 : 공모전 조회 후 db에 저장하는 로직 (but headless 적용 X)

---
+++ lambda/scrap/src/crawling/competitions.py
@@ -0,0 +1,148 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+from src.database.orm import ScrapType
+
+import time
+import json
+
+def safe_get_text(driver, by, value):
+    try:
+        return driver.find_element(by, value).text
+    except NoSuchElementException:
+        return '-'
+
+def safe_get_attr(driver, by, value, attr):
+    try:
+        return driver.find_element(by, value).get_attribute(attr)
+    except NoSuchElementException:
+        return '-'
+
+
+competitions_dict = dict()
+
+
+def get_href(driver):
+    results = []
+    page_count = 1
+    driver.get(f'https://linkareer.com/list/contest?filterType=CATEGORY&orderBy_direction=DESC&orderBy_field=CREATED_AT&page={page_count}')
+    time.sleep(1)
+
+    try:
+        while(True):
+            print(f" === 크롤링 중: {page_count} 페이지 === ")
+            cards = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "activity-list-card-item-wrapper")))
+            # 각 ACTIVITY당 썸네일에서 파싱 가능한 데이터는 미리 dictionary에 삽입 (key, value) -> 여기서 key값은 href의 마지막 숫자 ex) 243667, value는 dict()
+            for card in cards:
+                competitions_value = dict()
+                href = card.find_element(By.CLASS_NAME, "image-link").get_attribute("href")
+                results.append(href)
+                competitions_value["organizer_name"] = card.find_element(By.CLASS_NAME, "organization-name").text
+                competitions_value["title"] = card.find_element(By.CLASS_NAME, "activity-title").text
+                competition_key = href.split("/")[-1]
+                competitions_dict[competition_key] = competitions_value
+
+            # recruit_links = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "image-link")))
+            # hrefs = [recruit_link.get_attribute("href") for recruit_link in recruit_links]
+            # results.extend(hrefs)
+
+            buttons = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".button-page-number"))
+            )
+            max_number = 0
+            for btn in buttons:
+                max_number = max(max_number, int(btn.text.strip()))
+
+            active_button = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".active-page")))
+            active_number = int(active_button.text.strip())
+
+            # 다음 페이지 버튼 눌러야 할 때
+            if(active_number == max_number):
+                next_button = driver.find_element(By.CLASS_NAME, "button-arrow-next")
+                driver.execute_script("arguments[0].scrollIntoView(true);", next_button)
+
+                if "Mui-disabled" in next_button.get_attribute("class"):
+                    return results
+
+                next_button.click()
+
+            buttons = driver.find_elements(By.CSS_SELECTOR, ".button-page-number")
+            for btn in buttons:
+                if btn.text.strip() == str(active_number + 1):
+                    driver.execute_script("arguments[0].scrollIntoView(true);", btn)
+                    btn.click()
+                    break
+
+            page_count = active_number + 1
+    except Exception as e:
+        print(e)
+        driver.quit()
+        return {}
+
+
+def crawling_activities():
+    # 브라우저 꺼짐 방지
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option('detach', True)
+    User_Agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+    chrome_options.add_argument(f"user-agent={User_Agent}")
+    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+    hrefs = get_href(driver=driver)
+
+    for href in hrefs:
+        driver.get(href)
+        time.sleep(1)
+
+        competition_key = href.split("/")[-1]
+        competition_value = competitions_dict[competition_key]
+
+        competition_value["start_date"] = safe_get_text(driver, By.XPATH, "//span[@class='start-at']/following-sibling::span[1]")
+        competition_value["end_date"] = safe_get_text(driver, By.XPATH, "//span[@class='end-at']/following-sibling::span[1]")
+        try:
+            competition_value["job_category"] = json.dumps([elem.text for elem in driver.find_elements(By.XPATH, "//dt[text()='공모분야']/following-sibling::dd//li/p")], ensure_ascii=False)
+        except Exception:
+            competition_value["job_category"] = json.dumps([], ensure_ascii=False)
+        competition_value["homepage_url"] = safe_get_attr(driver, By.CSS_SELECTOR, "dd.text > a", "href")
+        competition_value["img_url"] = safe_get_attr(driver, By.CLASS_NAME, "card-image", "src")
+        competition_value["benefits"] = safe_get_text(driver, By.XPATH, "//dt[text()='활동혜택']/following-sibling::dd")
+        competition_value["additional_benefits"] = safe_get_text(driver, By.XPATH, "//dt[text()='추가혜택']/following-sibling::dd")
+        competition_value["participant"] = safe_get_text(driver, By.XPATH, "//dt[text()='참여대상']/following-sibling::dd")
+
+
+        save_to_db(competition_value)
+
+    driver.quit()
+
+def save_to_db(raw: dict) -> None:
+    from src.database.orm import Scrap, ScrapType
+    from datetime import datetime
+    scrap = Scrap(
+        scrap_type = ScrapType.INTERN,
+
+        title = raw["title"],
+        organizer_name = raw["organizer_name"],
+        start_date = raw["start_date"],
+        end_date = raw["end_date"],
+        job_category = raw["job_category"],
+        homepage_url = raw["homepage_url"],
+        img_url = raw["img_url"],
+        benefits = raw["benefits"],
+        additional_benefits = raw["additional_benefits"],
+        # participant = raw["participant"]
+    )
+    from src.database.connection import get_session
+
+    with get_session() as session:
+        session.add(scrap)
+
+crawling_activities()
