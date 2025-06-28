8807b9885a3a179ee3366be6b07417a94e801e1d
kite_U <yurry45@naver.com>
2025-06-28 17:40:59 +0900

    feature/#151: Crawling 코드 반환 타입 json으로 설정(S3에 저장)

--- lambda/scrap/activity.py
+++ lambda/scrap/activity.py
@@ -11,11 +11,13 @@ import boto3
 
 from dotenv import load_dotenv
 
-def crawling_intern_link(driver):
-    main_url = 'https://linkareer.com/list/intern'
+
+
+def crawling_activity_link():
+    main_url = 'https://linkareer.com/list/activity'
 
     driver.get(main_url)
-    time.sleep(2)
+    time.sleep(3)
 
     links = []
 
@@ -24,19 +26,13 @@ def crawling_intern_link(driver):
         while True:
             print(f"크롤링 중: {page} 페이지")
 
-            rows = driver.find_elements(By.CSS_SELECTOR, 'tbody tr')
+            contents = driver.find_elements(By.CSS_SELECTOR, 'div[class^="activity-list-card-item-wrapper"]')
 
-            for row in rows:
-                tds = row.find_elements(By.TAG_NAME, 'td')
-                if len(tds) > 1:
-                    title_cell = tds[1]
-                    href = title_cell.find_element(By.CSS_SELECTOR, 'a[href^="/activity/"]').get_attribute('href')
+            for content in contents:
+                href = content.find_element(By.CSS_SELECTOR, 'a[href^="/activity/"]').get_attribute('href')
+                full_link = "https://linkareer.com" + href if href.startswith("/") else href
 
-                    full_link = "https://linkareer.com" + href if href.startswith("/") else href
-
-                    links.append(full_link)
-                else:
-                    continue
+                links.append(full_link)
 
             # 다음 페이지
             try:
@@ -46,7 +42,7 @@ def crawling_intern_link(driver):
                 )
                 next_page_button.click()
                 page += 1
-                time.sleep(2)
+                time.sleep(3)
 
             except NoSuchElementException:
                 try:
@@ -56,7 +52,7 @@ def crawling_intern_link(driver):
                         break
                     next_arrow.click()
                     page += 1
-                    time.sleep(2)
+                    time.sleep(3)
                 except NoSuchElementException:
                     print("다음 버튼 없음. 종료합니다.")
                     break
@@ -71,11 +67,24 @@ def crawling_intern_link(driver):
         print(e)
 
     finally:
+        driver.quit()
         return links
 
 
 # 구체적인 내용 크롤링
-def crawling_intern_detail(link:str, driver) -> dict | None:
+def crawling_activity_detail(link: str) -> dict | None:
+    # 브라우저 꺼짐 방지
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option('detach', True)
+    User_Agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+    chrome_options.add_argument(f"user-agent={User_Agent}")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-notifications")
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    # 크롬 드라이버 생성 (자동 업데이트도 같이)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     # 예외 처리
     def safe_find_text(by, value) -> str | None:
@@ -86,22 +95,24 @@ def crawling_intern_detail(link:str, driver) -> dict | None:
 
     try:
         driver.get(link)
-        time.sleep(2)
+        time.sleep(3)
 
-        title=safe_find_text(By.CSS_SELECTOR, 'header.ActivityInformationHeader__StyledWrapper-sc-7bdaebe9-0 h1')
+        title = safe_find_text(By.CSS_SELECTOR, 'header.ActivityInformationHeader__StyledWrapper-sc-7bdaebe9-0 h1')
 
         prefix = "https://linkareer.com/activity/"
         linkareer_id = int(link.replace(prefix, ""))
 
-        img_url = driver.find_element(By.CSS_SELECTOR, 'img.recruit-image').get_attribute('src')
+        img_url = driver.find_element(By.CSS_SELECTOR, 'img.card-image').get_attribute('src')
 
         organizer_name = safe_find_text(By.CSS_SELECTOR, "h2.organization-name")
 
         enterprise_type = safe_find_text(By.XPATH, "//dl[dt[text()='기업형태']]/dd")
 
-        job_category = safe_find_text(By.XPATH, "//dl[dt[text()='모집직무']]/dd")
+        job_category = safe_find_text(By.XPATH, "//dl[dt[text()='관심분야']]/dd")
 
-        region = safe_find_text(By.XPATH, "//dl[dt[text()='근무지역']]/dd")
+        eligibility = safe_find_text(By.XPATH, "//dl[dt[text()='참여대상']]/dd")
+
+        benefits = safe_find_text(By.XPATH, "//dl[dt[text()='활동혜택']]/dd")
 
         raw_dates = safe_find_text(By.XPATH, "//dl[dt[text()='접수기간']]/dd")
 
@@ -122,21 +133,24 @@ def crawling_intern_detail(link:str, driver) -> dict | None:
             homepage_url = link
 
         return {
-            "title":title,
-            "linkareer_id":linkareer_id,
-            "organizer_name":organizer_name,
-            "img_url":img_url,
-            "enterprise_type":enterprise_type,
-            "job_category":job_category,
-            "region":region,
-            "start_date":start_date,
-            "end_date":end_date,
-            "homepage_url":homepage_url,
+            "title": title,
+            "linkareer_id": linkareer_id,
+            "organizer_name": organizer_name,
+            "img_url": img_url,
+            "enterprise_type": enterprise_type,
+            "job_category": job_category,
+            "eligibility": eligibility,
+            "benefits": benefits,
+            "start_date": start_date,
+            "end_date": end_date,
+            "homepage_url": homepage_url,
         }
 
     except Exception as e:
         print(f"상세 페이지 오류: {e}")
         return None
+    finally:
+        driver.quit()
 
 
 # S3에 저장
@@ -146,12 +160,12 @@ def lambda_handler(event, context):
 
     s3 = boto3.client("s3")
 
-    BUCKET_NAME = os.getenv("S3_BUCKET")
+    BUCKET_NAME = os.etenv("S3_BUCKET")
 
-    key = f"data/intern.json"
+    key = f"data/activity.json"
 
     try:
-        old_data = s3.get_object(Bucket = BUCKET_NAME, Key= key)
+        old_data = s3.get_object(Bucket=BUCKET_NAME, Key=key)
     except Exception as e:
         old_data = []
         print("파일을 읽을 수 없습니다.", e)
@@ -169,22 +183,19 @@ def lambda_handler(event, context):
     # 크롬 드라이버 생성 (자동 업데이트도 같이)
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-    links = crawling_intern_link(driver)
+    links = crawling_activity_link(driver)
 
-    new_data = []
     for link in links:
         if int(link.replace("https://linkareer.com/activity/", "")) == old_data["linkareer_id"]:
             continue
         else:
-            new_data.append(crawling_intern_detail(link, driver))
+            crawling_activity_detail(link, driver)
 
     s3.put_object(
-        Bucket=bucket_name,
+        Bucket=BUCKET_NAME,
         Key=key,
-        Body=json.dumps(new_data, indent=2),
-        ContentType="application/json"
+        Body=json.dumps(new_data)
     )
-
     return {
         "statusCode": 200,
         "body": json.dumps({"message": "Saved to S3", "s3_key": key})
