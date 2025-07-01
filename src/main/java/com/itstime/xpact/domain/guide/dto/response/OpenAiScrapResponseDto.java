d0eab0c8cc318cdbdcb6b8de51d088602ff7ee92
Wonjae Lim <yyytir777@gmail.com>
2025-07-02 00:16:28 +0900

    feature/#160 : openAi 요청 후 파싱할 dto클래스 생성

---
+++ src/main/java/com/itstime/xpact/domain/guide/dto/response/OpenAiScrapResponseDto.java
@@ -0,0 +1,11 @@
+package com.itstime.xpact.domain.guide.dto.response;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Getter
+public class OpenAiScrapResponseDto extends HashMap<String, List<Long>> {
+
+}
