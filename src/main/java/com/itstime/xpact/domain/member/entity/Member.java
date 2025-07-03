895a192ddecef6efac5cc8e7db1f5e998f38fe35
kite_U <yurry45@naver.com>
2025-07-03 15:28:20 +0900

    fix: Member 엔티티 education 필드 NPE 방지 위해 null 허용 처리

--- src/main/java/com/itstime/xpact/domain/member/entity/Member.java
+++ src/main/java/com/itstime/xpact/domain/member/entity/Member.java
@@ -108,12 +108,17 @@ public class Member extends BaseEntity {
                 .name(member.getName())
                 .imgurl(member.getImgurl() != null ? member.getImgurl() : null)
                 .age(member.getAge() != null ? member.getAge() : 0)
-                .educationName(member.getEducation() == null ? null : member.getEducation().getEducationName())
+                .educationName(
+                        member.getEducation() != null && member.getEducation().getEducationName() != null
+                                ? member.getEducation().getEducationName()
+                                : null
+                )
                 .desiredDetailRecruit(member.getDesiredRecruit() != null ? member.getDesiredRecruit() : null)
                 .educationSaveResponseDto(educationDto != null ? educationDto : null)
                 .desiredRecruitResponseDto(desiredRecruitDto != null ? desiredRecruitDto : null)
                 .build();
     }
+
 
     public void updateMemberInfo(MemberSaveRequestDto requestDto) {
         if (requestDto.name() != null) this.name = requestDto.name();
