9af0b1af9595934c9b97970ea483783431a6e768
Wonjae Lim <yyytir777@gmail.com>
2025-07-09 14:08:46 +0900

    feature : dashboard-scrap API 추가

--- src/main/java/com/itstime/xpact/domain/dashboard/service/DashboardService.java
+++ src/main/java/com/itstime/xpact/domain/dashboard/service/DashboardService.java
@@ -1,12 +1,12 @@
 package com.itstime.xpact.domain.dashboard.service;
 
-import com.itstime.xpact.domain.dashboard.controller.HistoryOldResponseDto;
-import com.itstime.xpact.domain.dashboard.dto.response.HistoryResponseDto;
-import com.itstime.xpact.domain.dashboard.dto.response.RatioResponseDto;
-import com.itstime.xpact.domain.dashboard.dto.response.TimelineResponseDto;
+import com.itstime.xpact.domain.dashboard.dto.response.*;
 import com.itstime.xpact.domain.dashboard.service.ratio.RatioService;
 import com.itstime.xpact.domain.dashboard.service.skillmap.SkillmapService;
 import com.itstime.xpact.domain.dashboard.service.time.TimeService;
+import com.itstime.xpact.domain.guide.entity.MemberScrap;
+import com.itstime.xpact.domain.guide.entity.Scrap;
+import com.itstime.xpact.domain.guide.repository.MemberScrapRepository;
 import com.itstime.xpact.domain.member.entity.Member;
 import com.itstime.xpact.global.auth.SecurityProvider;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +15,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,6 +32,8 @@ public class DashboardService {
     private final SkillmapService skillmapService;
     private final RatioService ratioService;
     private final TimeService timeService;
+
+    private final MemberScrapRepository memberScrapRepository;
 
     // 핵심스킬 맵
     public CompletableFuture<?> evaluate() {
@@ -69,4 +75,39 @@ public class DashboardService {
 
         return timeService.getTimeLine(member, startLine, endLine);
     }
+
+    @Transactional(readOnly = true)
+    public List<ScrapResponseDto> getScrapList() {
+        Member member = securityProvider.getCurrentMember();
+
+        return memberScrapRepository.findAllByMember(member).stream()
+                .map(MemberScrap::getScrap).toList().stream()
+                .filter(scrap -> canParseToLocalDate(scrap.getEndDate()))
+                .map(scrap -> {
+                    String dDay = getDday(scrap.getEndDate());
+                    return ScrapResponseDto.of(scrap, dDay);
+                }).toList();
+    }
+
+    private boolean canParseToLocalDate(String date) {
+        try {
+            LocalDate parse = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+            return parse.isBefore(LocalDate.now());
+        } catch (DateTimeParseException | NullPointerException e) {
+            return false;
+        }
+    }
+
+    private String getDday(String endDate) {
+        try {
+            LocalDate localDate = LocalDate.parse(endDate, DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+            int diff = (int) ChronoUnit.DAYS.between(localDate, LocalDate.now());
+
+            if(diff < 0) return "D-";
+            else return "D-" + diff;
+        } catch (DateTimeParseException | NullPointerException e) {
+            return "D-";
+        }
+    }
+
 }
